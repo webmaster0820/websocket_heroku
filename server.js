42fcb8eed05e8da88651e6915b5111a2655e7852
GOLD <goldtime0820@gmail.com>
2019-04-27 00:48:42 -0300

    Update

--- server.js
+++ server.js
@@ -13,51 +13,13 @@ const server = express()
 
 const wss = new SocketServer({ server });
 
-var wsArr = [];
-var PoolForInterval = [];
-var PoolForData = [];
-
-function send_data(userId,data){
-  if(PoolForData[userId] !== undefined && PoolForData[userId].length){
-    console.log("insert data to " + userId + "Pool");
-    PoolForData[userId].push(data);
-  }else{
-    console.log("create inerval for " + userId);
-    PoolForData[userId] = [];
-    PoolForData[userId].push(data);
-    createInterval(userId);
-  }
-}
-
-function createInterval(userId){
-  PoolForInterval[userId] = setInterval(function(){
-    if(PoolForData[userId].length){
-      console.log("send and remove data to " + userId + "Pool");
-      wsArr[userId].send(JSON.stringify(PoolForData[userId][0]));
-      PoolForData.shift();
-    }else{
-      console.log("remove interval " + userId);
-      clearInterval(PoolForInterval[userId]);
-    }
-  },1000);
-}
-
 wss.on('connection', (ws) => {
-
-  ws.on('message',(message)=>{
-    data = JSON.parse(message);
-    if (data.type == "connect"){
-      console.log("connected : " + data.userId);
-      wsArr[data.userId] = ws;
-      ws.send("connection success");
-    }
-    if (data.type == "send data"){
-      console.log("send data to : " + data.userId);
-      if (wsArr[data.userId] !== undefined){
-        send_data(data.userId, data.sendData);
-      }
-    }
-  });
   console.log('Client connected');
   ws.on('close', () => console.log('Client disconnected'));
 });
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);
